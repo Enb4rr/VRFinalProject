eaf422b38fa7a3ffb205cca566ec1999c81b100f
alonso <almear01@gmail.com>
2022-10-14 09:01:51 -0500

    Interface Set

---
+++ VRFinalProject/Assets/Scripts/Interface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+    [SerializeField] Canvas onPicked, onFloor;
+
+    private void Awake()
+    {
+        onPicked.gameObject.SetActive(false);
+    }
+
+    public void PickUp()
+    {
+        onFloor.gameObject.SetActive(false);
+        onPicked.gameObject.SetActive(true);
+    }
+
+    public void Drop()
+    {
+        onPicked.gameObject.SetActive(false);
+        onFloor.gameObject.SetActive(true);
+    }
+}
