c401d422fffab0f9387742c963cc2f7c2e5e239f
Nestor Cano <nestcano2971@gmail.com>
2022-11-03 01:07:01 -0500

    botones en el inicio

--- VRFinalProject/Assets/Scripts/ChangeTextChapter.cs
+++ VRFinalProject/Assets/Scripts/ChangeTextChapter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class ChangeTextChapter : MonoBehaviour
 {
 
     [SerializeField] string chapterText;
-    public Text displayText;
+    public TMP_Text displayText;
 
     void Start()
     {
