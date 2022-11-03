465f28a6d7901a655d942f883752404c93bbb1c7
Nestor Cano <nestcano2971@gmail.com>
2022-11-02 20:38:17 -0500

    Cambios de canvas

---
+++ VRFinalProject/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+
+    [SerializeField] public Canvas initialCanvas;
+    [SerializeField] public List<CanvasGroup> canvasList = new List<CanvasGroup>();
+    [SerializeField] public List<Button> buttonList = new List<Button>();
+
+    private void Awake()
+    {
+        initialCanvas = GetComponent<Canvas>();
+    }
+
+    public void DeactivateCanvas()
+    {
+        initialCanvas.gameObject.SetActive(false);
+    }
+
+
+}
